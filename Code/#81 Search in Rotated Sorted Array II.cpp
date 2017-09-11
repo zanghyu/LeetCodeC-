0c6faa6a4518912bd9691139e128c4b965ea25d0
eaves <bjfuzanghongyu@163.com>
2017-09-11 15:08:20 +0800

    Update #81 Search in Rotated Sorted Array II.cpp

--- Code/#81 Search in Rotated Sorted Array II.cpp
+++ Code/#81 Search in Rotated Sorted Array II.cpp
@@ -25,19 +25,16 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int left = 0, right =  nums.size()-1, mid;
-        while(left<=right)
-        {
+        while(left<=right){
             mid = (left + right) >> 1;
             if(nums[mid] == target) return true;
             // the only difference from the first one, trickly case, just updat left and right
             if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
-            else if(nums[left] <= nums[mid])
-            {
+            else if(nums[left] <= nums[mid]){
                 if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
                 else left = mid + 1; 
             }
-            else
-            {
+            else{
                 if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
                 else right = mid-1;
             }
